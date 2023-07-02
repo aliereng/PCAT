640bd1788055b02826ca05ddcb0fbd789f1f0b8f
aliereng <alierengonulcelen@gmail.com>
2023-07-02 21:57:33 +0300

    ejs is complete

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
 const express = require('express');
-const path = require('path');
+
 const port = 3000;
 const app = express();
+//TEMPLATE ENGINE
+app.set('view engine', 'ejs');
+
+//MIDDLEWARE
 app.use(express.static('public'));
+
+//ROUTES
 app.get('/', (req, res, next) => {
-  res.sendFile(path.resolve(__dirname, 'temp/index.html'));
+  res.render('index');
 });
+app.get('/about', (req, res, next) => {
+    res.render('about');
+  });
+  app.get('/add', (req, res, next) => {
+    res.render('add');
+  });
 
 app.listen(port, () => {
   console.log(`sunucu ${port} portunda başlatıldı`);
