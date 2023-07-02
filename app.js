7967a23bea631a710312f23a1fc36e2a81509801
aliereng <alierengonulcelen@gmail.com>
2023-07-02 20:15:39 +0300

    static file setting complete

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const express = require('express');
-const app = express();
+const path = require('path');
 const port = 3000;
+const app = express();
+app.use(express.static('public'));
+app.get('/', (req, res, next) => {
+  res.sendFile(path.resolve(__dirname, 'index/contact.html'));
+});
+
 app.listen(port, () => {
   console.log(`sunucu ${port} portunda başlatıldı`);
 });
