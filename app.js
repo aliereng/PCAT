c0c60f228bb5eab2102eb4aecfa2b7f0d6afafab
aliereng <alierengonulcelen@gmail.com>
2023-07-03 21:42:08 +0300

    image upload complete

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
+const fileUpload = require('express-fileupload');
+const fs = require('fs');
 const { connectDatabase } = require('./helpers/databaseHelper/dbHelpers');
-const Photo = require("./models/Photo");
+const Photo = require('./models/Photo');
 
 const port = 3000;
 const app = express();
@@ -12,13 +14,21 @@ app.set('view engine', 'ejs');
 
 //MIDDLEWARE
 app.use(express.static('public'));
-app.use(express.urlencoded({extended:true}))
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(fileUpload());
+
 //ROUTES
-app.get('/', async (req, res, next) => {
+app.get('/', async (req, res) => {
   const photos = await Photo.find();
-  res.render('index',{
-    photos
+  res.render('index', {
+    photos,
+  });
+});
+app.get('/photos/:id', async (req, res) => {
+  const photo = await Photo.findById(req.params.id);
+  res.render('photo', {
+    photo,
   });
 });
 app.get('/about', (req, res, next) => {
@@ -28,8 +38,29 @@ app.get('/add', (req, res, next) => {
   res.render('add');
 });
 app.post('/photos', async (req, res, next) => {
-  await Photo.create(req.body);
-  res.redirect("/");
+  let sampleFile;
+  let uploadPath;
+  const uploadDir = 'public/img/uploads';
+
+  if (!fs.existsSync(uploadDir)) {
+    fs.mkdirSync(uploadDir);
+  }
+  if (!req.files || Object.keys(req.files).length === 0) {
+    return res.status(400).send('No files were uploaded.');
+  }
+  req.files.image.name = req.files.image.md5 + "." + req.files.image.name.split(".")[1];
+
+  sampleFile = req.files.image;
+  uploadPath = __dirname + '/public/img/uploads/' + sampleFile.name;
+  req.body.image = sampleFile.name;
+  sampleFile.mv(uploadPath, async function(err) {
+    if (err)
+      return res.status(500).send(err);
+
+      await Photo.create(req.body);
+      res.redirect('/');
+  });
+  
 });
 
 app.listen(port, () => {
